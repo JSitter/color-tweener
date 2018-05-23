a4c744cb77ee1be02ba81d4db923a6f9c3995339
Justin Sitter <justin@jaytria.com>
2018-05-23 13:26:15 -0700

    finished gradient descent

---
+++ index.js
@@ -0,0 +1,100 @@
+// let color(color_one, color_two){
+//   this.color_one = color_one
+//   this.color_two = color_two
+// }
+
+const colorRange = (a, b) => {
+
+  // private props
+  var colorA = setColorA(a);
+  var colorB = setColorB(b);
+  var vecA = convertStringToDecVec(colorA)
+  var vecB = convertStringToDecVec(colorB)
+  var percent = 0;
+  var leadingHash = False
+
+
+  // private methods
+  function gradientDescent(percent){
+    outputColor = {}
+    for (let color in this.aVec){
+      //gets color at a certain percentage of the distance between the two input colors
+      let moveDir = (this.bVec[color] - this.aVec[color])/Math.abs(this.bVec[color] - this.aVec[color])
+      outputColor[color] = Math.abs(percent * (this.bVec[color]-this.aVec[color])) + moveDir * this.aVec[color]
+    }
+    return outputColor;
+  }
+
+  function convertStringToDecVec (color_hex){
+    r = color_hex.substring(0, 2);
+    g = color_hex.substring(2, 4);
+    b = color_hex.substring(4);
+    return [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
+  }
+
+  function convertHexVecToDec(color_vector){
+    colorDec = [];
+    for (color in color_vector){
+      colorDec.append(parseInt(color, 16));
+    }
+    return colorDec;
+  }
+
+  function convertDecVecToHex(color_vector){
+    colorHex = []
+    for(color in color_vector){
+      colorHex.append(color_vector[color].toString(16));
+    }
+  }
+
+  function convertVecToString(colorVec){
+    colorString = ""
+    for( let color in colorVec ){
+      colorString += colorVec[color];
+    }
+    return colorString;
+  }
+
+  // public
+  function setColorA(a) {
+    if(a.contains("#")){
+      this.leadingHash = True;
+    }
+    colorA = a.replace("#", "");
+  }
+
+  function setColorB(b){
+    if(b.contains("#")){
+      this.leadingHash = True;
+    }
+    colorB = b.replace("#", "");
+  }
+
+  function getColorAt(p) {
+    colorVec = gradientDescent(p);
+    colorString = "";
+    if(leadingHash){
+      colorString = "#";
+    }
+    colorString += convertVecToString(colorVec)
+  }
+
+  function getColorA() {
+    return colorA;
+  }
+
+  function getColorB() {
+    return colorB;
+  }
+
+  
+
+  // public methods
+  return {
+    getColorA,
+    getColorB,
+    getColorAt
+  }
+}
+
+module.exports = colorRange;
---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert();
+const app = require('../index')
+
+describe('App', function() {
+  describe('', function() {
+    it('should save without error', function(done) {
+
+
+      // (function(err) {
+      //   if (err) done(err);
+      //   else done();
+      // });
+
+    });
+  });
+});
